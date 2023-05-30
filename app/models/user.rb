9e45dd281c7ccab794d94c7473ea1e0c1316fc37
Fene-87 <fenekayas87@gmail.com>
2023-05-30 10:19:08 +0300

    Replace entity model with operation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+
+    has_many :groups
+    has_many :operations
 end
--- db/migrate/20230530071544_create_operations.rb
+++ db/migrate/20230530071544_create_operations.rb
@@ -1,6 +1,6 @@
-class CreateEntities < ActiveRecord::Migration[7.0]
+class CreateOperations < ActiveRecord::Migration[7.0]
   def change
-    create_table :entities do |t|
+    create_table :operations do |t|
       t.bigint :author_id
       t.string :name
       t.float :amount
