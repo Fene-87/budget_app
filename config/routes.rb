6666d26b5fde90c6094bc4b913262afa6d021898
Fene-87 <fenekayas87@gmail.com>
2023-05-31 17:35:59 +0300

    Enable user to add a new category

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'groups#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
-  resources :groups
+  resources :groups, only: [:index, :new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
