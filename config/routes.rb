2d51e1c1882387e3d0024c32c74315afd6908ee6
Fene-87 <fenekayas87@gmail.com>
2023-06-01 22:36:01 +0300

    Create splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
+
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'splash#splash', as: :unauthenticated_root
+  end
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
   resources :groups, path: 'categories', only: [:index, :new, :create] do
